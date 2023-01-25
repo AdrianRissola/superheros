c1c508778d24ecf42297232d92d3570179fc5e2c
Adrián R <adryanpr@gmail.com>
2023-01-25 17:35:51 -0300

    adding builders to domain entities

--- src/main/java/com/w2m/superheros/application/model/entities/Power.java
+++ src/main/java/com/w2m/superheros/application/model/entities/Power.java
@@ -1,58 +1,79 @@
 package com.w2m.superheros.application.model.entities;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import com.w2m.superheros.application.model.exceptions.SuperheroException;
+import com.w2m.superheros.helper.Utils;
+
 public class Power {
 
 	private int id;
 	
+	@NotBlank(message = "'name' is required")
 	private String name;
 
 	private int intensity;
 
-	private boolean isEnabled;
+	@NotNull(message = "'isEnabled' boolean field is required, allowed input: 'true' or f'alse'")
+	private Boolean isEnabled;
 
-    private Superhero superhero;
 	
-	public Power() {
+	private Power(int id, String name, int intensity, boolean isEnabled) {
+		this.id = id;
+		this.name = name;
+		this.intensity = intensity;
+		this.setEnabled(isEnabled);
+	}
+	
+	private void setEnabled(boolean isEnabled) {
+		this.isEnabled = isEnabled;
 	}
 
 	public int getId() {
 		return id;
 	}
 
-	public void setId(int id) {
-		this.id = id;
-	}
-
 	public String getName() {
 		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
 	}
 
 	public boolean isEnabled() {
 		return isEnabled;
 	}
-
-	public void setEnabled(boolean isEnabled) {
-		this.isEnabled = isEnabled;
-	}
-
-	public Superhero getSuperhero() {
-		return superhero;
-	}
-
-	public void setSuperhero(Superhero superhero) {
-		this.superhero = superhero;
-	}
-
+	
 	public int getIntensity() {
 		return intensity;
 	}
 
-	public void setIntensity(int intensity) {
-		this.intensity = intensity;
+	
+	public static class Builder {
+		
+		private int id;
+		
+		private String name;
+
+		private int intensity;
+
+		private boolean isEnabled;
+		
+		public Builder(String name, int intensity, boolean isEnabled) {
+			Utils.validateNullOrBlank(new SuperheroException("power 'name' is mandatory", null), name);
+			this.name = name;
+			this.intensity = intensity;
+			this.isEnabled = isEnabled;
+		}
+		
+		public Builder withId(int id) {
+			this.id = id;
+			return this;
+		}
+		
+		public Power build() {
+			return new Power(this.id, this.name, this.intensity, this.isEnabled);
+		}
+		
+		
 	}
 
 }
--- src/main/java/com/w2m/superheros/application/model/entities/Superhero.java
+++ src/main/java/com/w2m/superheros/application/model/entities/Superhero.java
@@ -3,65 +3,100 @@ package com.w2m.superheros.application.model.entities;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.http.HttpStatus;
+
+import com.w2m.superheros.application.model.exceptions.SuperheroException;
+import com.w2m.superheros.helper.Utils;
 
 
 public class Superhero {
 
 	private int id;
 	
-	@NotBlank(message = "name is required")
+	@NotBlank(message = "'name' is required for a superhero")
 	private String name;
 	
-	@NotBlank(message = "realFullName is required")
+	@NotBlank(message = "'realFullName' is required for a superhero")
 	private String realFullName;
 	
 	@NotNull(message = "'humanBeing' boolean field is required, allowed input: 'true' or f'alse'")
-	private boolean isHumanBeing;
+	private Boolean isHumanBeing;
 	
-	private List<Power> powers = new ArrayList<Power>();
+	@NotNull(message = "'powers' list is required for a superhero")
+	@NotEmpty(message = "'powers' list cannot be empty.")
+	@Size(min = 1, message = "a superhero shoud has at least uno superpower")
+	private List<
+		@Valid 
+		@NotNull(message = "'power' should not be null")
+		Power> powers;
 	
-	public Superhero(){}	
-
+	
+	private Superhero(int id, String name, String realFullName, boolean isHumanBeing, List<Power> powers){
+		this.id = id;
+		this.name = name;
+		this.realFullName = realFullName;
+		this.isHumanBeing = isHumanBeing;
+		this.powers = new ArrayList<>(powers);
+	}
+	
 	public int getId() {
 		return id;
 	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
+	
 	public String getName() {
 		return name;
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
 	public List<Power> getPowers() {
-		return powers;
-	}
-
-	public void setPowers(List<Power> powers) {
-		this.powers = powers;
+		return new ArrayList<>(this.powers);
 	}
 
 	public String getRealFullName() {
 		return realFullName;
 	}
 
-	public void setRealFullName(String realFullName) {
-		this.realFullName = realFullName;
-	}
-
 	public boolean isHumanBeing() {
 		return isHumanBeing;
 	}
-
-	public void setHumanBeing(boolean isHumanBeing) {
-		this.isHumanBeing = isHumanBeing;
+	
+	
+	public static class Builder {
+		
+		private int id;
+		
+		private String name;
+		
+		private String realFullName;
+		
+		private boolean isHumanBeing;
+		
+		private List<Power> powers;
+		
+		public Builder(String name, String realFullName, boolean isHumanBeing, List<Power> powers) {
+			Utils.validateNullOrBlank(
+					new SuperheroException("name and realFullName are mandatory", null), name, realFullName);
+			Utils.validateIsNullOrEmpty(powers, new SuperheroException(
+					"a superhero needs at least one superpower", HttpStatus.BAD_REQUEST));
+			this.name = name;
+			this.realFullName = realFullName;
+			this.isHumanBeing = isHumanBeing;
+			this.powers = powers;
+		}
+		
+		public Builder withId(int id) {
+			this.id = id;
+			return this;
+		}
+		
+		public Superhero build() {
+			return new Superhero(this.id, this.name, this.realFullName, this.isHumanBeing, this.powers);
+		}
 	}
 
 
